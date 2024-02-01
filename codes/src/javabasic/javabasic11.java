ee3e4568fdfc6c0d644f72b24a5310cc1b1dea3f
Aditya rai <raijiaditya9868@gmail.com>
2024-02-02 01:23:28 +0530

    test

---
+++ codes/src/javabasic/javabasic11.java
@@ -0,0 +1,9 @@
+public class javabasic11 {
+    public static void main(String arg[]){
+        System.out.println((3<2)&&(4>5)) ;/*logic operator(logical AND) */
+        System.out.println((1<2)&&(6<1));
+        System.out.println((2<3)&&(1<4));
+        System.out.println((3>4)||(1>2));/*logic OR */    
+    }
+    
+}
