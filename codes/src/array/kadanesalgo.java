e20e145113e0fda465f3532e740b2e238df2c5da
Aditya rai <raijiaditya9868@gmail.com>
2024-02-04 03:29:05 +0530

    kadanesalgo

--- codes/src/array/kadanesalgo.java
+++ codes/src/array/kadanesalgo.java
@@ -3,15 +3,12 @@ public class kadanesalgo {
         int ms = Integer.MIN_VALUE;
         int cs = 0;
         for(int i =0;i<a.length;i++){
-            cs+=a[i];
-            if (cs<0){
-                 cs=-1;
-            }
+            cs = Math.max(a[i], cs + a[i]);
             ms = Math.max(ms,cs);
         }System.out.println("the max sum = "+ ms);
     }
     public static void main(String[] args) {
-        int a[] = {-1,-2,-4,6};
+        int a[] = {-1,-2};
         kadanes(a);
     }
     
